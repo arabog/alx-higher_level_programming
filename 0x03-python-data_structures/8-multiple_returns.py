0391150369e1b2b06b4bc2a14920ee086bf7e004
arabog <arababs2015@gmail.com>
2022-12-06 09:54:24 -0800

    modify: 8-multiple_returns.py

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -5,6 +5,6 @@ def multiple_returns(sentence):
     if (len_sen == 0):
         new_tuple = (len_sen, None)
     else:
-        new_tuple = (len_sen, sentence)
+        new_tuple = (len_sen, sentence[0])
 
     return (new_tuple)

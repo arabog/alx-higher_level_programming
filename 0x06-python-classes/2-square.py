80ac0cd3bda33bbae6e66e43e3e566c846b9c434
arabog <arababs2015@gmail.com>
2022-12-22 03:21:43 +0100

    modify: 3-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
-"""Represents a square.
-Private instance attribute: size.
-Instantiation with optional size.
-"""
+# 0-square.py by Ehoneah Obed
+"""A module that defines a square """
 
 
 class Square:
-    """square with optional size"""
+    """A class that represents a square"""
+
     def __init__(self, size=0):
-        """Initializes the data."""
+        """Initializing this square class
+        Args:
+            size: represnets the size of the square defined
+        Raises:
+            TypeError: if size is not integer
+            ValueError: if size is less than zero
+        """
+
         if not isinstance(size, int):
-            raise TypeError("size must be an integer")
-        elif size < 0:
-            raise ValueError("size must be >= 0")
+            raise TypeError('size must be an integer')
+        if size < 0:
+            raise ValueError('size must be >= 0')
+
         self.__size = size

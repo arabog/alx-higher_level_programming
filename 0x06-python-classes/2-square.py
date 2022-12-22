d0469a327fed54c5395a1e45eb1e48a4e7e75a04
arabog <arababs2015@gmail.com>
2022-12-22 02:54:36 +0100

    modify: 3-square.py

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -4,15 +4,11 @@
 
 class Square:
     """ class Square that defines a square"""
-
-
-    def __init__(self, size=0):
-
+    def __init__(self, size):
         """initialize square
         Args:
             size (int): size of the square
         """
-
 
         if type(size) is not int:
             raise TypeError('size must be an integer')

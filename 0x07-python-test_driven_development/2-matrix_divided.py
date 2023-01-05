188969ea7e530236de671ff2dacf207fe08f0790
arabog <arababs2015@gmail.com>
2023-01-05 12:27:47 -0800

    modify: 2-matrix_divided.py

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,29 +6,24 @@ This module is composed by a function that divides the numbers of a matrix
 
 def matrix_divided(matrix, div):
     """ Function that divides the integer/float numbers of a matrix
-
     Args:
         matrix: list of a lists of integers/floats
         div: number which divides the matrix
-    
     Returns:
         A new matrix with the result of the division
-    
     Raises:
         TypeError: If the elements of the matrix aren't lists
                    If the elemetns of the lists aren't integers/floats
                    If div is not an integer/float number
                    If the lists of the matrix don't have the same size
-       
-       ZeroDivisionError: If div is zero
-    
+        ZeroDivisionError: If div is zero
     """
 
     if not type(div) in (int, float):
         raise TypeError("div must be a number")
 
     if div == 0:
-        raise ZeroDivisionError("can't divide by zero")
+        raise ZeroDivisionError("division by zero")
 
     msg_type = "matrix must be a matrix (list of lists) of integers/floats"
 
@@ -36,7 +31,6 @@ def matrix_divided(matrix, div):
         raise TypeError(msg_type)
 
     len_e = 0
-
     msg_size = "Each row of the matrix must have the same size"
 
     for elems in matrix:
@@ -44,7 +38,7 @@ def matrix_divided(matrix, div):
             raise TypeError(msg_type)
 
         if len_e != 0 and len(elems) != len_e:
-            raise TypeError(msg_type)
+            raise TypeError(msg_size)
 
         for num in elems:
             if not type(num) in (int, float):
@@ -52,7 +46,5 @@ def matrix_divided(matrix, div):
 
         len_e = len(elems)
 
-
     m = list(map(lambda x: list(map(lambda y: round(y / div, 2), x)), matrix))
-
     return (m)

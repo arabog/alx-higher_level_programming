bbe0ef78cca91c5ffdedf44ee9c2541c86b0f8a4
arabog <arababs2015@gmail.com>
2023-01-12 01:05:58 -0800

    modify: 7-rectangle.py

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -6,7 +6,7 @@
 class Rectangle:
     """ class rectangle"""
     number_of_instances = 0
-    print_symbol = '#'
+    print_symbol = "#"
 
     def __init__(self, width=0, height=0):
         """ Instantiation with optional width and height"""
@@ -22,7 +22,7 @@ class Rectangle:
 
     @property
     def height(self):
-        """height
+        """ height
         """
         return self.__height
 
@@ -62,7 +62,7 @@ class Rectangle:
         if self.__width is 0 or self.__height is 0:
             return ""
         return ("\n".join(["".join([str(self.print_symbol)
-            for i in range(self.__width)]) for j in range(self.__height)]))
+                for i in range(self.__width)]) for j in range(self.__height)]))
 
     def __repr__(self):
         """ return a string representation of the rectangle

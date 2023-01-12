2eddaac0db44fe6820be0c6325db70932df8cd76
arabog <arababs2015@gmail.com>
2023-01-11 23:57:32 -0800

    modify: 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-""" empty class Rectangle that defines a rectangle """
+""" empty class Rectangle that defines a rectangle
+"""
 
 
 class Rectangle:
@@ -11,17 +12,20 @@ class Rectangle:
 
     @property
     def width(self):
-        """ width"""
+        """ width
+        """
         return self.__width
 
     @property
     def height(self):
-        """height"""
+        """ height
+        """
         return self.__height
 
     @width.setter
     def width(self, value):
-        """ width setter """
+        """ width setter
+        """
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -30,7 +34,8 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """ height setter """
+        """ height setter
+        """
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
@@ -38,11 +43,11 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-         """ returns rectangle area"""
-         return self.__width * self.__height
+        """ returns rectangle area"""
+        return self.__width * self.__height
 
     def perimeter(self):
-         """ returns rectangle perimiter"""
-         if self.__width == 0 or self.__height == 0:
-             return 0
-         return self.__width * 2 + self.__height * 2
+        """ returns rectangle perimiter"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return self.__width * 2 + self.__height * 2

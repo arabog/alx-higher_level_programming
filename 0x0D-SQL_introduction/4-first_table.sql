c2ed1619b2a0b9d7789f37ea84021c9a271c6002
arabog <arababs2015@gmail.com>
2023-02-18 20:17:26 -0800

    modify: 3-list_tables.sql

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,3 @@
+-- creates a table in the current database if doesnt exist
+-- create id (INT) and name (VARCHAR(256)), database passed as argument
+CREATE TABLE IF NOT EXISTS `first_table` (`id` INT, `name` VARCHAR(256));
